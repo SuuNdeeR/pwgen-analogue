09af1d194e608804c680c5baeeee0c0a420a210b
anzarmix <andrey2502174@gmail.com>
2026-05-22 19:08:18 +0700

    Issue-2: отформатирован random.c

--- src/random.c
+++ src/random.c
@@ -34,7 +34,7 @@ int random_index(int max)
     }
 
     const uint32_t u_max = (uint32_t)max;
-    // Метод отбраковки (Rejection Sampling) для защиты от modulo bias 
+    // Метод отбраковки (Rejection Sampling) для защиты от modulo bias
     const uint32_t limit = UINT32_MAX - (UINT32_MAX % u_max);
     uint32_t val = 0;
 
