608cedf1aeff388eff124e1244e1a39916030754
anzarmix <andrey2502174@gmail.com>
2026-05-22 20:20:42 +0700

    Issue-3: реализован output.c/h

--- include/output.h
+++ include/output.h
@@ -1,6 +1,7 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+// вывод одного пароля в stdout с переводом строки
 void output_print(const char* password);
 
 #endif
--- src/output.c
+++ src/output.c
@@ -3,5 +3,8 @@
 
 void output_print(const char* password)
 {
+    if (password == NULL) {
+        return;
+    }
     printf("%s\n", password);
 }
